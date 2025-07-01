c4d45d8c86436781e13fd04732248869825367cd
Abhijeet Vikram <vikramabhijeet68@gmail.com>
2025-07-02 05:12:48 +0530

    Update portfolio-data.ts

--- src/lib/portfolio-data.ts
+++ src/lib/portfolio-data.ts
@@ -1,4 +1,4 @@
-
+import Image from "next/image";
 /**
  * =====================================================================================
  *                                  PORTFOLIO DATA
@@ -27,7 +27,7 @@ export const portfolioData = {
    */
   name: "Abhijeet Vikram",
   bio: 'Undergrad Data Science @ IISER Pune with interests in Deep Learning, Physics Inspired NNs and AI for Quantum.',
-  avatarUrl: "/profile.png",
+  avatarUrl: "/website/profile.png",
 
   /**
    * --- RESUME & CONTACT ---
